9c729477a5114aced6d9a69ca2c9d1b3f2bc09c7
Andy Park <apark1214@gmail.com>
2021-12-28 14:48:47 -0800

    create dataObject from images for each trim

---
+++ my-app/src/component/DataObject.js
@@ -0,0 +1,24 @@
+const carData = {
+  Premium: {
+    "Q4 40 e-tron": {
+      spec: {
+        engine: "Asynchronous electric motor",
+        hp: "201",
+        torque: "N/A",
+        acceleration: "7.9",
+        wheels: {
+          wheelImage: [],
+        },
+        standard: {
+          extImage: {
+            "Navarra Blue metallic": [
+              require("../img/Q440e-tron/NavarraBluemetallic/Audi-Q4-40-e-tron-back-angle-shot-Navarra-Blue-metallic.png")
+            ],
+          },
+        },
+      },
+    },
+  },
+};
+
+export default carData;
