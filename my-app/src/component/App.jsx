268f87d244659247f333e08e71cb354c4f3f200c
Andy Park <apark1214@gmail.com>
2021-12-29 10:32:40 -0800

    left and right page split

--- my-app/src/component/App.jsx
+++ my-app/src/component/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+
+//CSS Style
+import './App.css';
+
+//Components
 import {MainLeft} from '../component/mainPage/leftPage';
+import {MainRight} from '../component/mainPage/rightPage';
+
 
 export const App =()=>{
   return(
-    <div>
+    <div className="page-split">
       <MainLeft/>
+      <MainRight/>
     </div>
   );
 };
---
+++ my-app/src/component/mainPage/rightPage/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export const MainRight=()=>{
+  return(
+    <div className="audi-container">Hello</div>
+  )
+}
