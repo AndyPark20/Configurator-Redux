fab4cc2d4cb21e33c3ba24a8c9669fe685c6e9d4
Andy Park <apark1214@gmail.com>
2021-12-31 13:23:52 -0800

    working on carousel functionality

---
+++ my-app/src/component/mainPage/leftPage/gallery/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+
+export const Gallery=()=>{
+  return(
+    <div className="arrows">
+      <i class="fas fa-chevron-left"></i>
+      <i class="fas fa-chevron-right"></i>
+    </div>
+  )
+}
--- my-app/src/component/mainPage/leftPage/index.js
+++ my-app/src/component/mainPage/leftPage/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Data from "../../DataObject";
 
 //Connect functionality from React-Redux;
 import { connect } from 'react-redux';
@@ -7,21 +6,24 @@ import { connect } from 'react-redux';
 //CSS Styling
 import "./index.css";
 
-const MainLeft = (data) => {
-  //Destructre data
-    //==>For Current User Selection
-  const {level, trim,wheel_Selection,ext_Color} = data.currentSelection;
+//Import Components
 
-    //==>For carData
-    const {acceleration,engine,hp,torque} = data.carData[level][trim].spec;
+import {Gallery} from './gallery';
+
+const MainLeft = (data) => {
+
+  //Destructre data
+  //==>For Current User Selection
+  const { level, trim, wheel_Selection, ext_Color } = data.currentSelection;
+  //==>For carData
+  const { acceleration, engine, hp, torque } = data.carData[level][trim].spec;
 
   //Specification Render for Trim Selected
   const specRender = () => {
-
     return (
       <div className="spec">
         {console.log(data)}
-    <p><span className="spec-category-style">Engine Type: </span><span className="engine-type">{engine}</span></p>
+        <p><span className="spec-category-style">Engine Type: </span><span className="engine-type">{engine}</span></p>
         <p><span className="spec-category-style">Max. Output: </span><span className="engine-type">{hp} HP</span></p>
         <p><span className="spec-category-style">Max. Torque: </span><span className="engine-type">{torque} LB-FT</span></p>
         <p><span className="spec-category-style">Acceleration: </span><span className="engine-type">{acceleration} Seconds</span></p>
@@ -42,21 +44,21 @@ const MainLeft = (data) => {
               </span>
             </h1>
           </div>
-          <img
+          <Gallery/>
+          {/* <img
             src={data.carData[level][trim].spec[wheel_Selection].extImage[ext_Color][0]}
-            className="default-image" alt="Q4 40 e-tron Navarra Blue metallic" />
+            className="default-image" alt="Q4 40 e-tron Navarra Blue metallic" /> */}
         </div>
-          {specRender()}
+        {specRender()}
       </div>
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
-
   return {
     carData: state.carData,
-    currentSelection:state.userSelection
+    currentSelection: state.userSelection
   }
 };
 
