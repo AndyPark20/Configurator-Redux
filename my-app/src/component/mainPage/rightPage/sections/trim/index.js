50b1960b2e145c765544a6ef3f8b54e63515d81f
Andy Park <apark1214@gmail.com>
2021-12-30 16:57:23 -0800

    render trim and package

--- my-app/src/component/mainPage/rightPage/sections/trim/index.js
+++ my-app/src/component/mainPage/rightPage/sections/trim/index.js
@@ -3,16 +3,43 @@ import {connect} from 'react-redux';
 
 const Trim = ({ data }) => {
 
-  const RenderTrim = () => {
-    return <div>{console.log(data)}</div>;
+  // console.log(data)
+
+
+  //Render Trim Level
+  const renderTrimLevel =(values)=>{
+    if(data.carData.hasOwnProperty(values)){
+      return(
+        <div>
+          <h4>{values}</h4>
+          {console.log(data.carData[values])}
+          </div>
+        // <div>{data.carData[values]}</div>
+      )
+    }
+  }
+
+
+  //Render Package level
+  const renderPackageLevel = () => {
+    const packageLevel = Object.keys(data.carData);
+    const renderPackage = packageLevel.map((values,index)=>{
+      return (
+        <div>
+          {renderTrimLevel(values)}
+        </div>
+      );
+    })
+    return renderPackage;
   };
 
 
-  return <div>{RenderTrim()}</div>;
+  return <div>
+    {renderPackageLevel()}
+    </div>;
 };
 
 const thisMapStateToProps = (state) => {
-  console.log(state);
   return {
     data: state,
   };
