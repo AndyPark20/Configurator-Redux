3a110d25fb5ca9ff77977bc02cce498bd4e8c39f
Andy Park <apark1214@gmail.com>
2021-12-31 15:27:33 -0800

    working on Q4 premium 50 e-tron

--- my-app/src/reducers/index.js
+++ my-app/src/reducers/index.js
@@ -21,17 +21,40 @@ const carData = () => {
           standard_wheel_one: {
             extImage: {
               "Navarra Blue metallic": [
-                require("../img/Q440e-tron/NavarraBluemetallic/1-Audi-Q4-40-e-tron-Navarra-Blue-metallic.png"),
-                require("../img/Q440e-tron/NavarraBluemetallic/2-Audi-Q4-40-e-tron-Navarra-Blue-metallic.png"),
-                require("../img/Q440e-tron/NavarraBluemetallic/3-Audi-Q4-40-e-tron-Navarra-Blue-metallic.png"),
-                require("../img/Q440e-tron/NavarraBluemetallic/4-Audi-Q4-40-e-tron-Navarra-Blue-metallic.png"),
-                require("../img/Q440e-tron/NavarraBluemetallic/5-Audi-Q4-40-e-tron-Navarra-Blue-metallic.png"),
+                require("../img/premium/Q440e-tron/NavarraBluemetallic/2-Audi-Q4-40-e-tron-Navarra-Blue-metallic.png"),
+                require("../img/premium/Q440e-tron/NavarraBluemetallic/3-Audi-Q4-40-e-tron-Navarra-Blue-metallic.png"),
+                require("../img/premium/Q440e-tron/NavarraBluemetallic/4-Audi-Q4-40-e-tron-Navarra-Blue-metallic.png"),
+                require("../img/premium/Q440e-tron/NavarraBluemetallic/5-Audi-Q4-40-e-tron-Navarra-Blue-metallic.png"),
+                require("../img/premium/Q440e-tron/NavarraBluemetallic/1-Audi-Q4-40-e-tron-Navarra-Blue-metallic.png")
               ],
             },
           },
         },
       },
-      "Q4 50 e-tron": {},
+      "Q4 50 e-tron": {
+        spec:{
+          engine:"Rear Permanently excited synchronous motor and front asynchronous motor",
+          hp:"295",
+          torque:"N/A",
+          acceleration:"5.8",
+          wheels:{
+            wheelImage:[
+
+            ];
+          },
+          standard_wheel_one:{
+            extImage:{
+              "Navarra Blue metallic": [
+                require("../img/premium/1-standard-One-wheel-Audi-Q4-50-e-tron-Navarra-Blue-metallic.png"),
+                require("../img/premium/2-standard-One-wheel-Audi-Q4-50-e-tron-Navarra-Blue-metallicpng"),
+                require("../img/premium/3-standard-One-wheel-Audi-Q4-50-e-tron-Navarra-Blue-metallic.png"),
+                require("../img/premium/4-standard-One-wheel-Audi-Q4-50-e-tron-Navarra-Blue-metallic.png"),
+                require("../img/premium/5-standard-One-wheel-Audi-Q4-50-e-tron-Navarra-Blue-metallic.png")
+              ],
+            }
+          }
+        }
+      },
     },
     "Premium Plus": {
       "Q4 40 e-tron": {},
