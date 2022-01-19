f949a388ee15e798b9a00c6282707673358e37d2
Andy Park <apark1214@gmail.com>
2022-01-18 18:00:02 -0800

    will be working on interior next

--- my-app/src/component/mainPage/Leftpage/Gallery/Gallery.js
+++ my-app/src/component/mainPage/Leftpage/Gallery/Gallery.js
@@ -47,7 +47,7 @@ export const Gallery = ({ carData, gallery}) => {
       {renderGallery()}
       <div className="arrows">
         <i className="fas fa-chevron-left" onClick={(e) => changePictures(e)}></i>
-        {extGalleryIndex+1}/{gallery.length}
+        <span className="currentIndex">{extGalleryIndex+1}</span>/<span className="galleryLength">{gallery.length}</span>
         <i className="fas fa-chevron-right" onClick={(e) => changePictures(e)}></i>
       </div>
     </div>
