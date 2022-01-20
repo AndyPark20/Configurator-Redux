b2541e5f87b1a9139b0a61bbf53bf37f87558282
Andy Park <apark1214@gmail.com>
2022-01-19 18:34:34 -0800

    in progress

--- my-app/src/component/mainPage/rightPage/sections/Interior/Interior.js
+++ my-app/src/component/mainPage/rightPage/sections/Interior/Interior.js
@@ -1,43 +1,33 @@
-import React,{useState} from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from "react";
+import { connect } from "react-redux";
 
-import './Interior.css'
+import "./Interior.css";
 
-
-
-const InteriorSection =({data,trim,model,extColor})=>{
-
-
+const InteriorSection = ({ data, trim, model, extColor }) => {
   //Render Interior Button
-  const interiorButton =()=>{
-   const interiorColorButton =data[trim][model].spec.interior.intColorImage;
+  const interiorButton = () => {
+    const interiorColorButton = data[trim][model].spec.interior.intColorImage;
 
-   const renderInteriorBtn = interiorColorButton.map((values,index)=>{
-      return(
+    const renderInteriorBtn = interiorColorButton.map((values, index) => {
+      return (
         <div key={index}>
-          <img src={values.url} alt={values.name}/>
+          <img src={values.url} alt={values.name} />
         </div>
-      )
-   })
-  return renderInteriorBtn;
-  }
+      );
+    });
+    return renderInteriorBtn;
+  };
 
+  return <div>{interiorButton()}</div>;
+};
 
-  return(
-    <div>
-     {interiorButton()}
-    </div>
-  )
-}
-
-const mapStateToProps =(state)=>{
-  return{
-    data:state.carData,
-    trim:state.userSelection.trim,
-    model:state.userSelection.model,
-    extColor:state.userSelection.extColor
+const mapStateToProps = (state) => {
+  return {
+    data: state.carData,
+    trim: state.userSelection.trim,
+    model: state.userSelection.model,
+    extColor: state.userSelection.extColor,
   };
 };
 
-
-export default connect(mapStateToProps)(InteriorSection)
+export default connect(mapStateToProps)(InteriorSection);
