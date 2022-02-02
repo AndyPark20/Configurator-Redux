32015a24bdce55700dea3faa74d6d9f004ba180c
Andy Park <apark1214@gmail.com>
2022-02-01 17:40:04 -0800

    setting up connect function on mainright component

--- my-app/src/component/mainPage/rightPage/Rightpage.js
+++ my-app/src/component/mainPage/rightPage/Rightpage.js
@@ -6,13 +6,17 @@ import "./Rightpage.css";
 //Import Browser Router functionality
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+//Import Action creator
+import { userSelectedTrim } from "../../../Actions";
+
 //Import Components
 import Trim from "./Sections/Trim/Trim";
 import ExteriorSection from "./Sections/Exterior/Exterior";
 import InteriorSection from './Sections/Interior/Interior';
 import { RightSideHeader } from "../RightPage/Header/Header.js";
+import { connect } from "react-redux";
 
-export const MainRight = () => {
+const MainRight = () => {
   return (
     <div className="audi-container-right right-side-background">
       <BrowserRouter>
@@ -26,3 +30,12 @@ export const MainRight = () => {
     </div>
   );
 };
+
+const mapStateToProps =(state)=>{
+return{
+  state
+}
+}
+
+
+export default connect(mapStateToProps,{userSelectedTrim})(MainRight)
