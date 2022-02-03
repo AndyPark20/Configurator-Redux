39a7b9fe88563a7cf225b3318c237184617889ee
Andy Park <apark1214@gmail.com>
2022-02-02 16:12:47 -0800

    working on rendering out buckeet seats

--- my-app/src/component/mainPage/Leftpage/Leftpage.js
+++ my-app/src/component/mainPage/Leftpage/Leftpage.js
@@ -26,12 +26,21 @@ const MainLeft = ({
 
 
   const toggleImageSection = () => {
-  let exteriorPictures = carData[trim][model].spec[wheelSelection][viewPosition][extColor];
-   if(sectionView){
-   return exteriorPictures;
-   }
-   let interiorPictures=carData[trim][model].spec.interior.intImage[intColor][extColor];
-   return interiorPictures;
+    let exteriorPictures =
+      carData[trim][model].spec[wheelSelection][viewPosition][extColor];
+    if (sectionView) {
+      return exteriorPictures;
+    }
+
+    //Check to see if the selected wheel is for S-Line, if so render out bucket black seat interior images
+    //premium_wheel_two --> S-Line only package
+    let interiorPictures =carData[trim][model].spec.interior.intImage[intColor][extColor];
+    if(wheelSelection !=='premium_wheel_two'){
+        return interiorPictures;
+    }else{
+      console.log(intColor)
+    }
+
   };
 
   //Specification Render for Trim Selected
