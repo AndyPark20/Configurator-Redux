ebd175073cf2fb0a89008cd73a2f854079d4e036
Andy Park <apark1214@gmail.com>
2022-02-17 07:57:11 -0800

    completed building convenience options package for Q4 40 e-tron premium

--- my-app/src/reducers/index.js
+++ my-app/src/reducers/index.js
@@ -32,14 +32,14 @@ const carData = () => {
           acceleration: "7.9",
           layout: "rear-wheel drive",
           optionsPackages:[
-            {
-              "Convenience Package":{
-                image:
+              {"Convenience Package":{
+              image:require("../img/premium/Q440e-tron/Options/Convenience package.png"),
                description: ['Audi phone box light','Anti-theft alarm system','Acoustic front door windows','Logo projectors in exterior mirrors','Power front seats',
             '4-way power lumbar for front seats','Power tailgate with hands-free opening','Audi advanced key (keyless engine start/stop & entry - doors and trunk)','Auto-dimming, power-folding exterior mirrors',
           'Auto-dimming interior mirror with compass','Heated steering wheel','Memory for driver’s seat','SiriusXM® All Access service (w/ 3-month trial subscription)']
             }
-          ]},
+          }
+          ],
           wheels: {
             wheelImage: [
               {
