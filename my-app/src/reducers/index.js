0f3fc109de90602786ae4ee7ae4927af73054824
Andy Park <apark1214@gmail.com>
2022-02-22 16:42:50 -0800

    options rendering

--- my-app/src/reducers/index.js
+++ my-app/src/reducers/index.js
@@ -37,8 +37,13 @@ const carData = () => {
                description: ['Audi phone box light','Anti-theft alarm system','Acoustic front door windows','Logo projectors in exterior mirrors','Power front seats',
             '4-way power lumbar for front seats','Power tailgate with hands-free opening','Audi advanced key (keyless engine start/stop & entry - doors and trunk)','Auto-dimming, power-folding exterior mirrors',
           'Auto-dimming interior mirror with compass','Heated steering wheel','Memory for driver’s seat','SiriusXM® All Access service (w/ 3-month trial subscription)']
-            }
-          }
+            },
+          },
+            {"Garage door opener (Homelink®)":{
+                image:require('../img/premium/Q440e-tron/Options/Homelink.jpg'),
+                description:[]
+              },
+          },
           ],
           wheels: {
             wheelImage: [
