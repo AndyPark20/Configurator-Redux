7ce016e258652ca1e0c7bac021b6834b9189a424
Andy Park <apark1214@gmail.com>
2022-04-22 10:02:14 -0700

    render pricing when user clicks exterior header

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -2,6 +2,7 @@
 
 //Trim Selection
 export const userSelectedTrim = (trimSelection, model, exteriorColor,wheelSelection,interiorColor,sectionView, defaultOptions,selectedOptionCheck, trimBasePrice) => {
+  console.log('hello')
     return {
       type: "USER_SELECTION",
       payload: { trimSelection, model,exteriorColor,wheelSelection, interiorColor,sectionView,defaultOptions,selectedOptionCheck, price:trimBasePrice }
