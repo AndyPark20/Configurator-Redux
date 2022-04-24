6e4fd550361531594f946de8672a81d830eb48a1
Andy Park <apark1214@gmail.com>
2022-04-24 12:14:35 -0700

    creating Redux

---
+++ my-app/src/component/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+
+const Footer =()=>{
+  return(
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+const mapStateToProps =()=>{
+  return{}
+}
+
+export default(mapStateToProps)(Footer)
