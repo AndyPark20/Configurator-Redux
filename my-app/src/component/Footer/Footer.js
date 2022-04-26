e427df15dcb525fbb45bcf4baadab46251c0a6f0
Andy Park <apark1214@gmail.com>
2022-04-26 16:20:00 -0700

    adding html

--- my-app/src/component/Footer/Footer.js
+++ my-app/src/component/Footer/Footer.js
@@ -5,15 +5,23 @@ import "./Footer.css"
 
 
 const Footer =({optionedPrice})=>{
-  return(
-    <div>
-      <p className="footer-styling">Price as built:{optionedPrice}</p>
+  return (
+    <div className="footer-container">
+      <div class="row">
+        <div class="col model-trim">
+          <p>2022</p>
+        </div>
+        <div class="col model-price">
+            <p className="footer-styling">Price as built:{optionedPrice}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 const mapStateToProps =(state)=>{
   console.log('from Footer',state.userSelection.trim)
+  console.log('from FOOTER 2', state.userSelection.model)
   return {
     carData: state.carData,
     currentSelection: state.userSelection,
