a779c73b02370e1faff9c6ab6a4ef6d7edd5820e
Andy Park <apark1214@gmail.com>
2022-04-26 16:30:53 -0700

    styling footers by applying CSS class

--- my-app/src/component/Footer/Footer.js
+++ my-app/src/component/Footer/Footer.js
@@ -7,7 +7,7 @@ import "./Footer.css"
 const Footer =({optionedPrice,model,trim})=>{
   return (
     <div className="footer-container">
-      <div className="row">
+      <div className="row footer-layout">
         <div class="col model-trim">
           <p className="year">2022<span className="make">&nbsp; Audi</span></p>
           <p className="user-selected-trim">{`${model} ${trim}`}</p>
@@ -21,19 +21,9 @@ const Footer =({optionedPrice,model,trim})=>{
 }
 
 const mapStateToProps =(state)=>{
-  console.log('from Footer',state.userSelection.trim)
-  console.log('from FOOTER 2', state.userSelection.model)
   return {
-    carData: state.carData,
-    currentSelection: state.userSelection,
     trim: state.userSelection.trim,
     model: state.userSelection.model,
-    wheelSelection: state.userSelection.wheelSelection,
-    extColor: state.userSelection.extColor,
-    viewPosition: state.userSelection.viewPosition,
-    intColor: state.userSelection.intcolor,
-    sectionView: state.userSelection.sectionView,
-    price: state.userSelection.price,
     optionedPrice: state.userSelection.optionedPrice,
   };
 }
