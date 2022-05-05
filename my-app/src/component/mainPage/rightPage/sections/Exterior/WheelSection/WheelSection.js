6f051ca623c70d445c658e7f8437b627083962c5
Andy Park <apark1214@gmail.com>
2022-05-04 17:47:22 -0700

    reworking exteriors clicks to work

--- my-app/src/component/mainPage/rightPage/sections/Exterior/WheelSection/WheelSection.js
+++ my-app/src/component/mainPage/rightPage/sections/Exterior/WheelSection/WheelSection.js
@@ -17,7 +17,8 @@ const WheelSection = ({
   userSelectedTrim,
   intColor,
   sectionView,
-  wheelSelection
+  wheelSelection,
+  currentSelection,
 }) => {
   //Destructre data
   //For Current User Selection:
@@ -27,17 +28,13 @@ const WheelSection = ({
   const wheelList = carData[trim][model].spec.wheels.wheelImage;
 
   const renderWheelList = () => {
-
-
     //function to check if user selected premium wheel two
-    const checksLineWheel=(value)=>{
+    const checksLineWheel = (value) => {
       if (Object.keys(value).toString() === "premium_wheel_two") {
-
         return "Black with Rock Gray stitching S-Line®";
       }
       return intColor;
-    }
-
+    };
 
     const wheelArray = wheelList.map((value, index) => {
       return (
@@ -50,8 +47,11 @@ const WheelSection = ({
               model,
               extColor,
               Object.keys(value).toString(),
-              intColor =checksLineWheel(value),
-              sectionView
+              (intColor = checksLineWheel(value)),
+              sectionView,
+              carData[trim][model].spec.optionsPackages,
+              null,
+              currentSelection.price
             )
           }
         >
@@ -76,11 +76,12 @@ const WheelSection = ({
 
 const mapStateToProps=(state)=>{
   return {
-    userSelection:state.userSelection,
-    carData:state.carData,
-     intColor: state.userSelection.intcolor,
+    carData: state.carData,
+    userSelection: state.userSelection,
+    intColor: state.userSelection.intcolor,
     sectionView: state.userSelection.sectionView,
-     wheelSelection: state.userSelection.wheelSelection,
+    wheelSelection: state.userSelection.wheelSelection,
+    currentSelection: state.userSelection,
   };
 };
 
