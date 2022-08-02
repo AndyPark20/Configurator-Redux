6efb0c9674b5e0d802aa00fc75a9594fa2bd71d0
Andy Park <apark1214@gmail.com>
2022-08-02 14:59:37 -0700

    working on rendering list to the modal:

--- my-app/src/component/OptionsModal/OptionsModal.js
+++ my-app/src/component/OptionsModal/OptionsModal.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-export const OptionsDetail =()=>{
+import './OptionsModal.css';
+
+export const OptionsDetail =({userSelection})=>{
   return(
     <div className="option-detail-modal">
-
+      {console.log('from const', userSelection)}
     </div>
   )
 }
 
 const thisMapStateToProps =(state)=>{
   return{
-
+    userSelection: state.userSelection
   }
 }
 
