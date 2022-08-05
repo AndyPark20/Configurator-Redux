b3437489fffbe88103f5db35be24cbb00d57b76b
Andy Park <apark1214@gmail.com>
2022-08-05 08:13:21 -0700

    using hashmap to select user selected option

--- my-app/src/component/OptionsModal/OptionsModal.js
+++ my-app/src/component/OptionsModal/OptionsModal.js
@@ -1,42 +1,28 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import { render } from "@testing-library/react";
+import React from "react";
+import { connect } from "react-redux";
 
-import './OptionsModal.css';
+import "./OptionsModal.css";
 
-export const OptionsDetail =({userSelection, modal,selectedOption})=>{
-
-  const optionList =()=>{
-    if(modal){
- console.log("userSelection", userSelection.selectedOptions);
- console.log('selectedOption', selectedOption)
- const renderOptionList = userSelection.selectedOptions.map((values,index)=>{
-
- })
+export const OptionsDetail = ({ userSelection, modal, selectedOption }) => {
+  const optionList = () => {
+    if (modal) {
+      const renderOptionList = userSelection.selectedOptions.map(
+        (values, innerIndex) => {
+          return <div>{console.log(values.option[])}</div>;
+        }
+      );
+      return renderOptionList;
     }
-      // if(userSelection){
-      //   const renderOptionList = userSelection.selectedOptions.map((value,index)=>{
-      //     return(
-      //       <div>
-      //         {console.log('values',value)}
-      //       </div>
-      //     )
-      //   })
-      //   renderOptionList();
-      // };
+  };
 
-  }
+  return <div className="option-detail-modal">{optionList()}</div>;
+};
 
-  return(
-    <div className="option-detail-modal">
-      {optionList()}
-    </div>
-  )
-}
+const thisMapStateToProps = (state) => {
+  return {
+    userSelection: state.userSelection,
+  };
+};
 
-const thisMapStateToProps =(state)=>{
-  return{
-    userSelection: state.userSelection
-  }
-}
-
-export default connect(thisMapStateToProps)(OptionsDetail)
+export default connect(thisMapStateToProps)(OptionsDetail);
