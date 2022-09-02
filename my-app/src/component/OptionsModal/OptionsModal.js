5f31cb8ff21cd8867218b203af06f382492d7f7a
Andy Park <apark1214@gmail.com>
2022-09-02 07:47:14 -0700

    hashmap completed

--- my-app/src/component/OptionsModal/OptionsModal.js
+++ my-app/src/component/OptionsModal/OptionsModal.js
@@ -32,7 +32,7 @@ export const OptionsDetail = ({ userSelection, modal, selectedOption, optionTitl
               <div className="row">
                 <div className="col">
                   <h2>{Object.keys(values.option[selectedOption])[0]}</h2>
-                  {console.log(Object.values(values.option[selectedOption]))}
+                  {console.log(Object.values(values.option[selectedOption])[0].image)}
                 </div>
                 <div className="col">{renderOptionDescription()}</div>
                 <div className="col">
